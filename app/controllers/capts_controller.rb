6f7d931fd1fe20bfae6d57b82c4a7ccfb09e7f2e
kingsley McSimon Ogbonna <kingsley.mcsimon@gmail.com>
2020-07-29 00:03:54 -0700

    some changes implemented in the controller file

--- app/controllers/capts_controller.rb
+++ app/controllers/capts_controller.rb
@@ -5,60 +5,25 @@ class CaptsController < ApplicationController # rubocop:todo Style/Documentation
 
   before_action :authenticate_user!, except: %i[index show]
 
-  # GET /capts
-
-  # GET /capts.json
-
   def index
     @capts = Capt.all.order('created_at DESC')
 
     @capt = Capt.new
   end
 
-  # GET /capts/1
-
-  # GET /capts/1.json
-
   def show; end
-
-  # GET /capts/new
 
   def new
     @capt = current_user.capts.build
   end
-
-  # GET /capts/1/edit
-
+  
   def edit
     @capt = Capt.find(params[:id]) 
 end
 
-  # POST /capts
-
-  # POST /capts.json
-
-  # def create # rubocop:todo Metrics/AbcSize
-  #   @capt = current_user.capts.build(capt_params)
-
-  #   respond_to do |format|
-  #     if @capt.save
-
-  #       format.html { redirect_to root_path, notice: 'Capt was successfully created.' }
-
-  #       format.json { render :show, status: :created, location: @capt }
-
-  #     else
-
-  #       format.html { render :new }
-
-  #       format.json { render json: @capt.errors, status: :unprocessable_entity }
-
-  #     end
-  #   end
-  # end
-
   def create
   @capt = Capt.new(capt_params)
+  @capt.user_id = current_user.id
    if @capt.save
     flash[:notice] = 'Post was successfully created'
     redirect_to capts_path(@capt)
@@ -66,28 +31,6 @@ end
     render 'new'
    end
 end
-
-  # PATCH/PUT /capts/1
-
-  # PATCH/PUT /capts/1.json
-
-  # def update
-  #   respond_to do |format|
-  #     if @capt.update(capt_params)
-
-  #       format.html { redirect_to @capt, notice: 'Capt was successfully updated.' }
-
-  #       format.json { render :show, status: :ok, location: @capt }
-
-  #     else
-
-  #       format.html { render :edit }
-
-  #       format.json { render json: @capt.errors, status: :unprocessable_entity }
-
-  #     end
-  #   end
-  # end
 
 def update
     @capt = Capt.find(params[:id])
@@ -98,10 +41,6 @@ def update
         render 'edit'
     end
 end
-
-  # DELETE /capts/1
-
-  # DELETE /capts/1.json
 
   def destroy
     @capt.destroy
