c5fc3cee0c69ccbf83ebd5d68452b685000028d8
Kingsley McSimon O <kingsley.mcsimon@gmail.com>
2020-08-01 01:07:25 -0700

    helper methods file updated

--- app/helpers/capts_helper.rb
+++ app/helpers/capts_helper.rb
@@ -10,23 +10,28 @@ def user_signed_in
 end
 
 def signed_in
-  render partial: 'trends' if user_signed_in? 
+if user_signed_in?
+  render partial: 'trends'
+render 'profile'
+ else
+ render 'trends'
+end
 end
 
 def password_length
-  render: 'password_length' f.input :password, label: "New password", required: true, autofocus: true, input_html: { class: "input"}, wrapper: false, label_html: { class: "label" }, hint: ("#{@minimum_password_length} characters minimum" if @minimum_password_length)
+  render 'password_length'  if @minimum_password_length
 end
 
 def devise_mapping
-  render: 'devise_mapping' if devise_mapping.confirmable? && resource.pending_reconfirmation?
+  render 'devise_mapping' if devise_mapping.confirmable? && resource.pending_reconfirmation?
 end
 
 def minimum_password
-  render: 'minimum_password' f.input :password, required: true, input_html: { class: "input" }, wrapper: false, label_html: { class: "label" }, hint: ("#{@minimum_password_length} characters minimum" if @minimum_password_length)
+  render 'minimum_password' if @minimum_password_length
 end
 
 def remember_me
-  render: 'remember_me' f.input :remember_me, wrapper: false, as: :boolean if devise_mapping.rememberable?
+  render 'remember_me' if devise_mapping.rememberable?
 end
 
 def resource_errors
@@ -34,27 +39,27 @@ def resource_errors
 end
 
 def controller_name
-  render: 'controller_name' if controller_name != 'sessions'
+  render 'controller_name' if controller_name != 'sessions'
 end
 
 def registerable
-  render: 'registerable' if devise_mapping.registerable? && controller_name != 'registrations'
+  render 'registerable' if devise_mapping.registerable? && controller_name != 'registrations'
 end
 
 def recoverable
-  render: 'recoverable' if devise_mapping.recoverable? && controller_name != 'passwords' && controller_name != 'registrations'
+  render 'recoverable' if devise_mapping.recoverable? && controller_name != 'passwords' && controller_name != 'registrations'
 end
 
 def confirmable
-  render: 'confirmable'  if devise_mapping.confirmable? && controller_name != 'confirmations'
+  render 'confirmable'  if devise_mapping.confirmable? && controller_name != 'confirmations'
 end
 
 def lockable
-  render: 'lockable'  if devise_mapping.lockable? && resource_class.unlock_strategy_enabled?(:email) && controller_name != 'unlocks'
+  render 'lockable'  if devise_mapping.lockable? && resource_class.unlock_strategy_enabled?(:email) && controller_name != 'unlocks'
 end
 
 def omniauthable
-  render: if devise_mapping.omniauthable?
+  render 'omniauthable' if devise_mapping.omniauthable?
 end
 
 def body_flash
@@ -66,7 +71,7 @@ def user_logged_in
 end
 
 def object_errors
-  render: f.error_notification message: f.object.errors[:base].to_sentence if f.object.errors[:base].present?
+  render 'object_errors' if f.object.errors[:base].present?
 end
 
 end
